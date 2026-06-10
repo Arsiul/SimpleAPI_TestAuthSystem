a7941dca92ee1404b3177e67862a627a2f04d891
Ars_Dev <fernandobarrantes802@gmail.com>
2026-06-10 01:51:53 -0500

    feat: autentication middleware added

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { userController } from '../controllers/user.controller.js';
+import { authMiddleware } from 'myauthsystem';
 import Router from 'express';
 
 const router = Router();
 
 router.post("/register", userController.createUser);
 router.post("/login", userController.login);
-router.get("/users", userController.getAllUsers);
+router.get("/users", authMiddleware(process.env.SECRET_KEY), userController.getAllUsers);
 
 export default router;
